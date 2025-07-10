dabeaff70e512614bb659a416f9bc5b404ab3931
Josip Šimun Kuči <josipsimunkuci@gmail.com>
2025-07-10 13:28:28 +0200

    Rework examples and update github actions

--- examples/ContinousMeasurmentWithDetails/ContinousMeasurmentWithDetails.ino
+++ examples/ContinousMeasurmentWithDetails/ContinousMeasurmentWithDetails.ino
@@ -1,52 +1,51 @@
 /**
  **************************************************
  *
- * @file        ReadDistance.ino
+ * @file        ReadDistanceContinous.ino
  * @brief       Exmaple that shows how to use VL53_L1X library with VL53L1X sensor to read distance, best to use in low
- *ambient light setting to get better results
- *
- *
- *
- * @authors     @ soldered.com, Robert Peric
+ *              ambient light setting to get better results
+ * @authors     kevin-pololu, github: https://github.com/pololu
  * @link        www.solde.red/333064
  ***************************************************/
 
 #include "VL53L1X-SOLDERED.h"
-#include "Wire.h"
 
-VL53_L1X myVl;
+VL53_L1X sensor;
 
 void setup()
 {
     Serial.begin(115200);
     Serial.println("Serial Initialised");
 
-    myVl.setTimeout(500);
-    myVl.begin();
+    sensor.setTimeout(500);
+    sensor.begin();
 
     // Use long distance mode and allow up to 50000 us (50 ms) for a measurement.
     // You can change these settings to adjust the performance of the sensor, but
     // the minimum timing budget is 20 ms for short distance mode and 33 ms for
     // medium and long distance modes. See the VL53L1X datasheet for more
     // information on range and timing limits.
-    myVl.setDistanceMode(VL53L1X::Long);
-    myVl.setMeasurementTimingBudget(50000);
+    sensor.setDistanceMode(VL53L1X::Long);
+    sensor.setMeasurementTimingBudget(50000);
 
     // Start continuous readings at a rate of one measurement every 50 ms (the
     // inter-measurement period). This period should be at least as long as the
     // timing budget.
-    myVl.startContinuous(50);
+    sensor.startContinuous(50);
 }
 
 void loop()
 {
-    Serial.print(myVl.read());  //Print reading
-    if (myVl.timeoutOccurred()) //Check if sensor has been measuring longer than timeout period
-    {
-        Serial.print(" TIMEOUT");
-    }
+  sensor.read();
 
-    Serial.println();
+  Serial.print("range: ");
+  Serial.print(sensor.ranging_data.range_mm);
+  Serial.print("\tstatus: ");
+  Serial.print(VL53L1X::rangeStatusToString(sensor.ranging_data.range_status));
+  Serial.print("\tpeak signal: ");
+  Serial.print(sensor.ranging_data.peak_signal_count_rate_MCPS);
+  Serial.print("\tambient: ");
+  Serial.print(sensor.ranging_data.ambient_count_rate_MCPS);
 
-    delay(3000);
+  Serial.println();
 }
